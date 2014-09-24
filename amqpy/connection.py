401be566504ff388dcde2fa2eb6296fce2a9c1bb
veegee <veegee@veegee.org>
2014-09-24 00:18:12 -0400

    Reformat comments

--- amqpy/connection.py
+++ amqpy/connection.py
@@ -418,12 +418,8 @@ class Connection(AbstractChannel):
         """Confirm a connection close
 
         This method confirms a Connection.Close method and tells the recipient that it is safe to release resources for
-        the connection and close the socket.
-
-        RULE:
-
-            A peer that detects a socket closure without having received a Close-Ok handshake method SHOULD log the
-            error.
+        the connection and close the socket. RULE: A peer that detects a socket closure without having received a
+        Close-Ok handshake method SHOULD log the error.
         """
         self._send_method(spec.Connection.CloseOk)
         self._do_close()
@@ -431,14 +427,9 @@ class Connection(AbstractChannel):
     def _close_ok(self, args):
         """Confirm a connection close
 
-        This method confirms a Connection.Close method and tells the
-        recipient that it is safe to release resources for the
-        connection and close the socket.
-
-        RULE:
-
-            A peer that detects a socket closure without having
-            received a Close-Ok handshake method SHOULD log the error.
+        This method confirms a Connection.Close method and tells the recipient that it is safe to release resources for
+        the connection and close the socket. A peer that detects a socket closure without having received a Close-Ok
+        handshake method SHOULD log the error.
         """
         self._do_close()
 
@@ -446,39 +437,13 @@ class Connection(AbstractChannel):
         """Open connection to virtual host
 
         This method opens a connection to a virtual host, which is a collection of resources, and acts to separate
-        multiple application domains within a server.
+        multiple application domains within a server. RULE: The client MUST open the context before doing any work on
+        the connection.
 
-        RULE:
-
-            The client MUST open the context before doing any work on the connection.
-
-        PARAMETERS: virtual_host: shortstr
-
-                virtual host name
-
-                The name of the virtual host to work with.
-
-                RULE:
-
-                    If the server supports multiple virtual hosts, it MUST enforce a full separation of exchanges,
-                    queues, and all associated entities per virtual host. An application, connected to a specific
-                    virtual host, MUST NOT be able to access resources of another virtual host.
-
-                RULE:
-
-                    The server SHOULD verify that the client has permission to access the specified virtual host.
-
-                RULE:
-
-                    The server MAY configure arbitrary limits per virtual host, such as the number of each type of
-                    entity that may be used, per connection and/or in total.
-
-            capabilities: shortstr
-
-                required capabilities
-
-                The client may specify a number of capability names, delimited by spaces.  The server can use this
-                string to how to process the client's connection request.
+        :param virtual_host: virtual host path
+        :param capabilities: required capabilities
+        :type virtual_host: str
+        :type capabilities: str
         """
         args = AMQPWriter()
         args.write_shortstr(virtual_host)
@@ -490,11 +455,7 @@ class Connection(AbstractChannel):
     def _open_ok(self, args):
         """Signal that the connection is ready
 
-        This method signals to the client that the connection is ready
-        for use.
-
-        PARAMETERS:
-            known_hosts: shortstr (deprecated)
+        This method signals to the client that the connection is ready for use.
         """
         log.debug('Open OK')
 
@@ -504,13 +465,12 @@ class Connection(AbstractChannel):
         The SASL protocol works by exchanging challenges and responses until both peers have received sufficient
         information to authenticate each other.  This method challenges the client to provide more information.
 
-        PARAMETERS: challenge: longstr
-
+        PARAMETERS:
+            challenge: longstr
                 security challenge data
-
                 Challenge information, a block of opaque binary data passed to the security mechanism.
         """
-        challenge = args.read_longstr()  # noqa
+        challenge = args.read_longstr()
 
     def _x_secure_ok(self, response):
         """Security mechanism response
@@ -518,11 +478,10 @@ class Connection(AbstractChannel):
         This method attempts to authenticate, passing a block of SASL data for the security mechanism at the server
         side.
 
-        PARAMETERS: response: longstr
-
+        PARAMETERS:
+            response: longstr
                 security response data
-
-                A block of opaque data passed to the security mechanism.  The contents of this data are defined by the
+                A block of opaque data passed to the security mechanism. The contents of this data are defined by the
                 SASL security mechanism.
         """
         args = AMQPWriter()
@@ -535,51 +494,33 @@ class Connection(AbstractChannel):
         This method starts the connection negotiation process by telling the client the protocol version that the server
         proposes, along with a list of security mechanisms which the client can use for authentication.
 
-        RULE:
+        RULE: If the client cannot handle the protocol version suggested by the server it MUST close the socket
+        connection.
 
-            If the client cannot handle the protocol version suggested by the server it MUST close the socket
-            connection.
+        RULE: The server MUST provide a protocol version that is lower than or equal to that requested by the client in
+        the protocol header. If the server cannot support the specified protocol it MUST NOT send this method, but MUST
+        close the socket connection.
 
-        RULE:
-
-            The server MUST provide a protocol version that is lower than or equal to that requested by the client in
-            the protocol header. If the server cannot support the specified protocol it MUST NOT send this method, but
-            MUST close the socket connection.
-
-        PARAMETERS: version_major: octet
-
+        PARAMETERS:
+            version_major: octet
                 protocol major version
-
                 The protocol major version that the server agrees to use, which cannot be higher than the client's major
                 version.
-
             version_minor: octet
-
                 protocol major version
-
                 The protocol minor version that the server agrees to use, which cannot be higher than the client's minor
                 version.
-
             server_properties: table
-
                 server properties
-
             mechanisms: longstr
-
                 available security mechanisms
-
                 A list of the security mechanisms that the server supports, delimited by spaces.  Currently ASL supports
                 these mechanisms: PLAIN.
-
             locales: longstr
-
                 available message locales
-
                 A list of the message locales that the server supports, delimited by spaces.  The locale defines the
                 language in which the server will send reply texts.
-
                 RULE:
-
                     All servers MUST support at least the en_US locale.
         """
         self.version_major = args.read_octet()
@@ -599,38 +540,25 @@ class Connection(AbstractChannel):
         This method selects a SASL security mechanism. ASL uses SASL (RFC2222) to negotiate authentication and
         encryption.
 
-        PARAMETERS: client_properties: table
-
+        PARAMETERS:
+            client_properties: table
                 client properties
-
             mechanism: shortstr
-
                 selected security mechanism
-
                 A single security mechanisms selected by the client, which must be one of those specified by the server.
-
                 RULE:
-
                     The client SHOULD authenticate using the highest- level security profile it can handle from the list
                     provided by the server.
-
                 RULE:
-
                     The mechanism field MUST contain one of the security mechanisms proposed by the server in the Start
                     method. If it doesn't, the server MUST close the socket.
-
             response: longstr
-
                 security response data
-
                 A block of opaque data passed to the security mechanism. The contents of this data are defined by the
                 SASL security mechanism.  For the PLAIN security mechanism this is defined as a field table holding two
                 fields, LOGIN and PASSWORD.
-
             locale: shortstr
-
                 selected message locale
-
                 A single message local selected by the client, which must be one of those specified by the server.
         """
         if self.server_capabilities.get('consumer_cancel_notify'):
@@ -650,35 +578,25 @@ class Connection(AbstractChannel):
 
     def _tune(self, args):
         """Propose connection tuning parameters
-
         This method proposes a set of connection configuration values to the client.  The client can accept and/or
         adjust these.
 
-        PARAMETERS: channel_max: short
-
+        PARAMETERS:
+            channel_max: short
                 proposed maximum channels
-
                 The maximum total number of channels that the server allows per connection. Zero means that the server
                 does not impose a fixed limit, but the number of allowed channels may be limited by available server
                 resources.
-
             frame_max: long
-
                 proposed maximum frame size
-
                 The largest frame size that the server proposes for the connection. The client can negotiate a lower
                 value.  Zero means that the server does not impose any specific limit but may reject very large frames
                 if it cannot allocate resources for them.
-
                 RULE:
-
                     Until the frame-max has been negotiated, both peers MUST accept frames of up to 4096 octets large.
                     The minimum non-zero value for the frame- max field is 4096.
-
             heartbeat: short
-
                 desired heartbeat delay
-
                 The delay, in seconds, of the connection heartbeat that the server wants.  Zero means the server does
                 not want a heartbeat.
         """
@@ -709,35 +627,24 @@ class Connection(AbstractChannel):
         This method sends the client's connection tuning parameters to the server. Certain fields are negotiated, others
         provide capability information.
 
-        PARAMETERS: channel_max: short
-
+        PARAMETERS:
+            channel_max: short
                 negotiated maximum channels
-
                 The maximum total number of channels that the client will use per connection.  May not be higher than
                 the value specified by the server.
-
                 RULE:
-
                     The server MAY ignore the channel-max value or MAY use it for tuning its resource allocation.
-
             frame_max: long
-
                 negotiated maximum frame size
-
                 The largest frame size that the client and server will use for the connection.  Zero means that the
                 client does not impose any specific limit but may reject very large frames if it cannot allocate
                 resources for them. Note that the frame-max limit applies principally to content frames, where large
                 contents can be broken into frames of arbitrary size.
-
                 RULE:
-
                     Until the frame-max has been negotiated, both peers must accept frames of up to 4096 octets large.
                     The minimum non-zero value for the frame- max field is 4096.
-
             heartbeat: short
-
                 desired heartbeat delay
-
                 The delay, in seconds, of the connection heartbeat that the client wants. Zero means the client does not
                 want a heartbeat.
         """
