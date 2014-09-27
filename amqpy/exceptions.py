666f0131aa8a6cfcbe0385b732cc99f002ba723a
veegee <veegee@veegee.org>
2014-09-27 13:31:25 -0400

    Rename method name strings according to spec
    They are lowercase and dash (-) instead of underscores (_) in the spec document.

--- amqpy/exceptions.py
+++ amqpy/exceptions.py
@@ -28,15 +28,25 @@ class Timeout(Exception):
 class AMQPError(Exception):
     code = 0
 
-    def __init__(self, reply_text=None, method_sig=None, method_name=None, reply_code=None):
+    def __init__(self, reply_text=None, method_type=None, method_name=None, reply_code=None):
+        """
+        :param reply_text: localized reply text
+        :param method_type: method type
+        :param method_name: method name
+        :param reply_code: AMQP reply (exception) code
+        :type reply_text: str
+        :type method_type: amqpy.spec.method_t
+        :type method_name: str
+        :type reply_code: int
+        """
         self.message = reply_text
         self.reply_code = reply_code or self.code
         self.reply_text = reply_text
-        self.method_sig = method_sig
+        self.method_sig = method_type
         self.method_name = method_name or ''
-        if method_sig and not self.method_name:
-            self.method_name = METHOD_NAME_MAP.get(method_sig, '')
-        Exception.__init__(self, reply_code, reply_text, method_sig, self.method_name)
+        if method_type and not self.method_name:
+            self.method_name = METHOD_NAME_MAP.get(method_type, '')
+        super().__init__(self, reply_code, reply_text, method_type, self.method_name)
 
     def __str__(self):
         if self.method:
@@ -169,81 +179,91 @@ ERROR_MAP = {
 }
 
 
-def error_for_code(code, text, method, default):
+def error_for_code(code, text, method_type, default):
+    """Get exception class associated with specified error code
+
+    :param code: AMQP reply code
+    :param text: localized reply text
+    :param method_type: method type
+    :param default: default exception class if error code cannot be matched with an exception class
+    :type code: int
+    :type text: str
+    :type method_type: amqpy.spec.method_t
+    :type default: Exception
+    :return: Exception object
+    :rtype: Exception
+    """
     try:
-        return ERROR_MAP[code](text, method, reply_code=code)
+        return ERROR_MAP[code](text, method_type, reply_code=code)
     except KeyError:
-        return default(text, method, reply_code=code)
-
-
-def raise_for_code(code, text, method, default):
-    raise error_for_code(code, text, method, default)
+        return default(text, method_type, reply_code=code)
 
 
 METHOD_NAME_MAP = {
-    method_t(10, 10): 'Connection.start',
-    method_t(10, 11): 'Connection.start_ok',
-    method_t(10, 20): 'Connection.secure',
-    method_t(10, 21): 'Connection.secure_ok',
-    method_t(10, 30): 'Connection.tune',
-    method_t(10, 31): 'Connection.tune_ok',
-    method_t(10, 40): 'Connection.open',
-    method_t(10, 41): 'Connection.open_ok',
-    method_t(10, 50): 'Connection.close',
-    method_t(10, 51): 'Connection.close_ok',
-    method_t(20, 10): 'Channel.open',
-    method_t(20, 11): 'Channel.open_ok',
-    method_t(20, 20): 'Channel.flow',
-    method_t(20, 21): 'Channel.flow_ok',
-    method_t(20, 40): 'Channel.close',
-    method_t(20, 41): 'Channel.close_ok',
-    method_t(30, 10): 'Access.request',
-    method_t(30, 11): 'Access.request_ok',
-    method_t(40, 10): 'Exchange.declare',
-    method_t(40, 11): 'Exchange.declare_ok',
-    method_t(40, 20): 'Exchange.delete',
-    method_t(40, 21): 'Exchange.delete_ok',
-    method_t(40, 30): 'Exchange.bind',
-    method_t(40, 31): 'Exchange.bind_ok',
-    method_t(40, 40): 'Exchange.unbind',
-    method_t(40, 51): 'Exchange.unbind_ok',
-    method_t(50, 10): 'Queue.declare',
-    method_t(50, 11): 'Queue.declare_ok',
-    method_t(50, 20): 'Queue.bind',
-    method_t(50, 21): 'Queue.bind_ok',
-    method_t(50, 30): 'Queue.purge',
-    method_t(50, 31): 'Queue.purge_ok',
-    method_t(50, 40): 'Queue.delete',
-    method_t(50, 41): 'Queue.delete_ok',
-    method_t(50, 50): 'Queue.unbind',
-    method_t(50, 51): 'Queue.unbind_ok',
-    method_t(60, 10): 'Basic.qos',
-    method_t(60, 11): 'Basic.qos_ok',
-    method_t(60, 20): 'Basic.consume',
-    method_t(60, 21): 'Basic.consume_ok',
-    method_t(60, 30): 'Basic.cancel',
-    method_t(60, 31): 'Basic.cancel_ok',
-    method_t(60, 40): 'Basic.publish',
-    method_t(60, 50): 'Basic.return',
-    method_t(60, 60): 'Basic.deliver',
-    method_t(60, 70): 'Basic.get',
-    method_t(60, 71): 'Basic.get_ok',
-    method_t(60, 72): 'Basic.get_empty',
-    method_t(60, 80): 'Basic.ack',
-    method_t(60, 90): 'Basic.reject',
-    method_t(60, 100): 'Basic.recover_async',
-    method_t(60, 110): 'Basic.recover',
-    method_t(60, 111): 'Basic.recover_ok',
-    method_t(60, 120): 'Basic.nack',
-    method_t(90, 10): 'Tx.select',
-    method_t(90, 11): 'Tx.select_ok',
-    method_t(90, 20): 'Tx.commit',
-    method_t(90, 21): 'Tx.commit_ok',
-    method_t(90, 30): 'Tx.rollback',
-    method_t(90, 31): 'Tx.rollback_ok',
-    method_t(85, 10): 'Confirm.select',
-    method_t(85, 11): 'Confirm.select_ok',
+    method_t(10, 10): 'connection.start',
+    method_t(10, 11): 'connection.start-ok',
+    method_t(10, 20): 'connection.secure',
+    method_t(10, 21): 'connection.secure-ok',
+    method_t(10, 30): 'connection.tune',
+    method_t(10, 31): 'connection.tune-ok',
+    method_t(10, 40): 'connection.open',
+    method_t(10, 41): 'connection.open-ok',
+    method_t(10, 50): 'connection.close',
+    method_t(10, 51): 'connection.close-ok',
+    method_t(20, 10): 'channel.open',
+    method_t(20, 11): 'channel.open-ok',
+    method_t(20, 20): 'channel.flow',
+    method_t(20, 21): 'channel.flow-ok',
+    method_t(20, 40): 'channel.close',
+    method_t(20, 41): 'channel.close-ok',
+    method_t(30, 10): 'access.request',
+    method_t(30, 11): 'access.request-ok',
+    method_t(40, 10): 'exchange.declare',
+    method_t(40, 11): 'exchange.declare-ok',
+    method_t(40, 20): 'exchange.delete',
+    method_t(40, 21): 'exchange.delete-ok',
+    method_t(40, 30): 'exchange.bind',
+    method_t(40, 31): 'exchange.bind-ok',
+    method_t(40, 40): 'exchange.unbind',
+    method_t(40, 51): 'exchange.unbind-ok',
+    method_t(50, 10): 'queue.declare',
+    method_t(50, 11): 'queue.declare-ok',
+    method_t(50, 20): 'queue.bind',
+    method_t(50, 21): 'queue.bind-ok',
+    method_t(50, 30): 'queue.purge',
+    method_t(50, 31): 'queue.purge-ok',
+    method_t(50, 40): 'queue.delete',
+    method_t(50, 41): 'queue.delete-ok',
+    method_t(50, 50): 'queue.unbind',
+    method_t(50, 51): 'queue.unbind-ok',
+    method_t(60, 10): 'basic.qos',
+    method_t(60, 11): 'basic.qos-ok',
+    method_t(60, 20): 'basic.consume',
+    method_t(60, 21): 'basic.consume-ok',
+    method_t(60, 30): 'basic.cancel',
+    method_t(60, 31): 'basic.cancel-ok',
+    method_t(60, 40): 'basic.publish',
+    method_t(60, 50): 'basic.return',
+    method_t(60, 60): 'basic.deliver',
+    method_t(60, 70): 'basic.get',
+    method_t(60, 71): 'basic.get-ok',
+    method_t(60, 72): 'basic.get-empty',
+    method_t(60, 80): 'basic.ack',
+    method_t(60, 90): 'basic.reject',
+    method_t(60, 100): 'basic.recover-async',
+    method_t(60, 110): 'basic.recover',
+    method_t(60, 111): 'basic.recover-ok',
+    method_t(60, 120): 'basic.nack',
+    method_t(90, 10): 'tx.select',
+    method_t(90, 11): 'tx.select-ok',
+    method_t(90, 20): 'tx.commit',
+    method_t(90, 21): 'tx.commit-ok',
+    method_t(90, 30): 'tx.rollback',
+    method_t(90, 31): 'tx.rollback-ok',
+    method_t(85, 10): 'confirm.select',
+    method_t(85, 11): 'confirm.select-ok',
 }
 
 for method_type, name in list(METHOD_NAME_MAP.items()):
-    METHOD_NAME_MAP[struct.unpack('>I', struct.pack('>HH', *method_type))[0]] = name
+    data = struct.pack('>HH', *method_type)
+    METHOD_NAME_MAP[struct.unpack('>I', data)[0]] = name
