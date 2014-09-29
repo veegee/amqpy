edae8302dba91de89e6ca71158cd93af44a7b2d8
veegee <veegee@veegee.org>
2014-09-29 18:05:53 -0400

    Fix docstring formatting

--- amqpy/message.py
+++ amqpy/message.py
@@ -119,20 +119,20 @@ class Message(GenericContent):
 
         `properties` can include:
 
-            * content_type (shortstr): MIME content type
-            * content_encoding (shortstr): MIME content encoding
-            * application_headers: (table): Message header field table: dict[str, str|int|Decimal|datetime|dict]
-            * delivery_mode: (octet): Non-persistent (1) or persistent (2)
-            * priority (octet): The message priority, 0 to 9
-            * correlation_id (shortstr) The application correlation identifier
-            * reply_to (shortstr) The destination to reply to
-            * expiration (shortstr): Message expiration specification
-            * message_id (shortstr): The application message identifier
-            * timestamp (datetime.datetime): The message timestamp
-            * type (shortstr): The message type name
-            * user_id (shortstr): The creating user id
-            * app_id (shortstr): The creating application id
-            * cluster_id (shortstr): Intra-cluster routing identifier
+        * content_type (shortstr): MIME content type
+        * content_encoding (shortstr): MIME content encoding
+        * application_headers: (table): Message header field table: dict[str, str|int|Decimal|datetime|dict]
+        * delivery_mode: (octet): Non-persistent (1) or persistent (2)
+        * priority (octet): The message priority, 0 to 9
+        * correlation_id (shortstr) The application correlation identifier
+        * reply_to (shortstr) The destination to reply to
+        * expiration (shortstr): Message expiration specification
+        * message_id (shortstr): The application message identifier
+        * timestamp (datetime.datetime): The message timestamp
+        * type (shortstr): The message type name
+        * user_id (shortstr): The creating user id
+        * app_id (shortstr): The creating application id
+        * cluster_id (shortstr): Intra-cluster routing identifier
 
         example::
 
