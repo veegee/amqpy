bf1a1aafd89824ebac27ddfce9f1ba101e14c71e
veegee <veegee@veegee.org>
2014-10-10 02:57:27 -0400

    Reorganize tests

--- amqpy/tests/test_channel.py
+++ amqpy/tests/test_channel.py
@@ -4,14 +4,97 @@ import sys
 
 import pytest
 
-from .. import Channel, Message, FrameSyntaxError
-from ..spec import queue_declare_ok_t
+from .. import Channel, Message, FrameSyntaxError, queue_declare_ok_t
 from ..exceptions import AMQPError, ChannelError, PreconditionFailed, NotFound, AccessRefused
 from .conftest import get_server_props
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, style='{', format='{asctime} {levelname:8} {message}',
                     datefmt='%Y/%m/%d %H:%M:%S')
 log = logging.getLogger('amqpy')
+
+
+class TestChannel:
+    def test_encoding(self, ch, rand_rk):
+        qname, _, _ = ch.queue_declare()
+        ch.queue_bind(qname, 'amq.direct', routing_key=rand_rk)
+
+        # no encoding, body passed through unchanged
+        msg = Message('hello world')
+        ch.basic_publish(msg, 'amq.direct', routing_key=rand_rk)
+        msg2 = ch.basic_get(qname, no_ack=True)
+        assert isinstance(msg2.body, str)
+        assert msg2.body == 'hello world'
+
+        # default UTF-8 encoding of unicode body, returned as unicode
+        msg = Message('hello world')
+        ch.basic_publish(msg, 'amq.direct', routing_key=rand_rk)
+        msg2 = ch.basic_get(qname, no_ack=True)
+        assert msg2.properties['content_encoding'] == 'UTF-8'
+        assert msg2.body == 'hello world'
+
+        # Explicit latin_1 encoding, still comes back as unicode
+        msg = Message('hello world', content_encoding='latin_1')
+        ch.basic_publish(msg, 'amq.direct', routing_key=rand_rk)
+        msg2 = ch.basic_get(qname, no_ack=True)
+        assert msg2.properties['content_encoding'] == 'latin_1'
+        assert msg2.body == 'hello world'
+
+        # plain string with specified encoding comes back as unicode
+        msg = Message('hello w\xf6rld', content_encoding='latin_1')
+        ch.basic_publish(msg, 'amq.direct', routing_key=rand_rk)
+        msg2 = ch.basic_get(qname, no_ack=True)
+
+        assert msg2.properties['content_encoding'] == 'latin_1'
+        assert msg2.body == 'hello w\u00f6rld'
+
+        # plain string (bytes in Python 3.x) with bogus encoding
+        # don't really care about latin_1, just want bytes
+        test_bytes = 'hello w\xd6rld'.encode('latin_1')
+        msg = Message(test_bytes, content_encoding='I made this up')
+        ch.basic_publish(msg, 'amq.direct', routing_key=rand_rk)
+        msg2 = ch.basic_get(qname, no_ack=True)
+        assert msg2.properties['content_encoding'] == 'I made this up'
+        assert isinstance(msg2.body, bytes)
+        assert msg2.body == test_bytes
+
+        # turn off auto_decode for remaining tests
+        ch.auto_decode = False
+
+        # unicode body comes back as utf-8 encoded str
+        msg = Message('hello w\u00f6rld')
+        ch.basic_publish(msg, 'amq.direct', routing_key=rand_rk)
+        msg2 = ch.basic_get(qname, no_ack=True)
+        assert msg2.properties['content_encoding'] == 'UTF-8'
+        assert isinstance(msg2.body, bytes)
+        assert msg2.body == 'hello w\xc3\xb6rld'.encode('latin_1')
+
+        # plain string with specified encoding stays plain string
+        msg = Message('hello w\xf6rld', content_encoding='latin_1')
+        ch.basic_publish(msg, 'amq.direct', routing_key=rand_rk)
+        msg2 = ch.basic_get(qname, no_ack=True)
+        assert msg2.properties['content_encoding'] == 'latin_1'
+        assert isinstance(msg2.body, bytes)
+        assert msg2.body == 'hello w\xf6rld'.encode('latin_1')
+
+        # explicit latin_1 encoding, comes back as str
+        msg = Message('hello w\u00f6rld', content_encoding='latin_1')
+        ch.basic_publish(msg, 'amq.direct', routing_key=rand_rk)
+        msg2 = ch.basic_get(qname, no_ack=True)
+        assert msg2.properties['content_encoding'] == 'latin_1'
+        assert isinstance(msg2.body, bytes)
+        assert msg2.body == 'hello w\xf6rld'.encode('latin_1')
+
+    def test_invalid_header(self, ch):
+        """Test sending a message with an unserializable object in the header
+
+        http://code.google.com/p/py-amqplib/issues/detail?id=17
+        """
+        qname, _, _ = ch.queue_declare()
+
+        msg = Message(application_headers={'test': object()})
+
+        with pytest.raises(FrameSyntaxError):
+            ch.basic_publish(msg, routing_key=qname)
 
 
 class TestExchange:
@@ -225,8 +308,20 @@ class TestPublish:
         ok = ch_b.queue_declare(rand_queue, passive=True)
         assert ok.message_count == 2
 
+    def test_basic_return(self, ch, rand_exch):
+        ch.exchange_declare(rand_exch, 'fanout')
 
-class TestChannel:
+        msg = Message('funtest message', content_type='text/plain', application_headers={'foo': 7, 'bar': 'baz'})
+
+        ch.basic_publish(msg, rand_exch)
+        ch.basic_publish(msg, rand_exch, mandatory=True)
+        ch.basic_publish(msg, rand_exch, mandatory=True)
+        ch.basic_publish(msg, rand_exch, mandatory=True)
+        ch.close()
+
+        # 3 of the 4 messages we sent should have been returned
+        assert ch.returned_messages.qsize() == 3
+
     def test_defaults(self, ch):
         """Test how a queue defaults to being bound to an AMQP default exchange, and how publishing defaults to the
         default exchange, and basic_get defaults to getting from the most recently declared queue, and queue_delete
@@ -246,107 +341,9 @@ class TestChannel:
         n = ch.queue_delete()
         assert n == 0
 
-    def test_encoding(self, ch):
-        my_routing_key = 'funtest.test_queue'
-
-        qname, _, _ = ch.queue_declare()
-        ch.queue_bind(qname, 'amq.direct', routing_key=my_routing_key)
-
-        # no encoding, body passed through unchanged
-        msg = Message('hello world')
-        ch.basic_publish(msg, 'amq.direct', routing_key=my_routing_key)
-        msg2 = ch.basic_get(qname, no_ack=True)
-        assert isinstance(msg2.body, str)
-        assert msg2.body == 'hello world'
-
-        # default UTF-8 encoding of unicode body, returned as unicode
-        msg = Message('hello world')
-        ch.basic_publish(msg, 'amq.direct', routing_key=my_routing_key)
-        msg2 = ch.basic_get(qname, no_ack=True)
-        assert msg2.properties['content_encoding'] == 'UTF-8'
-        assert msg2.body == 'hello world'
-
-        # Explicit latin_1 encoding, still comes back as unicode
-        msg = Message('hello world', content_encoding='latin_1')
-        ch.basic_publish(msg, 'amq.direct', routing_key=my_routing_key)
-        msg2 = ch.basic_get(qname, no_ack=True)
-        assert msg2.properties['content_encoding'] == 'latin_1'
-        assert msg2.body == 'hello world'
-
-        # plain string with specified encoding comes back as unicode
-        msg = Message('hello w\xf6rld', content_encoding='latin_1')
-        ch.basic_publish(msg, 'amq.direct', routing_key=my_routing_key)
-        msg2 = ch.basic_get(qname, no_ack=True)
-
-        assert msg2.properties['content_encoding'] == 'latin_1'
-        assert msg2.body == 'hello w\u00f6rld'
-
-        # plain string (bytes in Python 3.x) with bogus encoding
-        # don't really care about latin_1, just want bytes
-        test_bytes = 'hello w\xd6rld'.encode('latin_1')
-        msg = Message(test_bytes, content_encoding='I made this up')
-        ch.basic_publish(msg, 'amq.direct', routing_key=my_routing_key)
-        msg2 = ch.basic_get(qname, no_ack=True)
-        assert msg2.properties['content_encoding'] == 'I made this up'
-        assert isinstance(msg2.body, bytes)
-        assert msg2.body == test_bytes
-
-        # turn off auto_decode for remaining tests
-        ch.auto_decode = False
-
-        # unicode body comes back as utf-8 encoded str
-        msg = Message('hello w\u00f6rld')
-        ch.basic_publish(msg, 'amq.direct', routing_key=my_routing_key)
-        msg2 = ch.basic_get(qname, no_ack=True)
-        assert msg2.properties['content_encoding'] == 'UTF-8'
-        assert isinstance(msg2.body, bytes)
-        assert msg2.body == 'hello w\xc3\xb6rld'.encode('latin_1')
-
-        # plain string with specified encoding stays plain string
-        msg = Message('hello w\xf6rld', content_encoding='latin_1')
-        ch.basic_publish(msg, 'amq.direct', routing_key=my_routing_key)
-        msg2 = ch.basic_get(qname, no_ack=True)
-        assert msg2.properties['content_encoding'] == 'latin_1'
-        assert isinstance(msg2.body, bytes)
-        assert msg2.body == 'hello w\xf6rld'.encode('latin_1')
-
-        # explicit latin_1 encoding, comes back as str
-        msg = Message('hello w\u00f6rld', content_encoding='latin_1')
-        ch.basic_publish(msg, 'amq.direct', routing_key=my_routing_key)
-        msg2 = ch.basic_get(qname, no_ack=True)
-        assert msg2.properties['content_encoding'] == 'latin_1'
-        assert isinstance(msg2.body, bytes)
-        assert msg2.body == 'hello w\xf6rld'.encode('latin_1')
-
     def test_survives_channel_error(self, ch):
         with pytest.raises(ChannelError):
             ch.queue_declare('krjqheewq_bogus', passive=True)
         ch.queue_declare('funtest_survive')
         ch.queue_declare('funtest_survive', passive=True)
         assert ch.queue_delete('funtest_survive') == 0
-
-    def test_invalid_header(self, ch):
-        """Test sending a message with an unserializable object in the header
-
-        http://code.google.com/p/py-amqplib/issues/detail?id=17
-        """
-        qname, _, _ = ch.queue_declare()
-
-        msg = Message(application_headers={'test': object()})
-
-        with pytest.raises(FrameSyntaxError):
-            ch.basic_publish(msg, routing_key=qname)
-
-    def test_basic_return(self, ch):
-        ch.exchange_declare('funtest.fanout', 'fanout', auto_delete=True)
-
-        msg = Message('funtest message', content_type='text/plain', application_headers={'foo': 7, 'bar': 'baz'})
-
-        ch.basic_publish(msg, 'funtest.fanout')
-        ch.basic_publish(msg, 'funtest.fanout', mandatory=True)
-        ch.basic_publish(msg, 'funtest.fanout', mandatory=True)
-        ch.basic_publish(msg, 'funtest.fanout', mandatory=True)
-        ch.close()
-
-        # 3 of the 4 messages we sent should have been returned
-        assert ch.returned_messages.qsize() == 3
