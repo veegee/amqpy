7896e2bb053388a7177d761c9f65f5f6b8b41dc5
veegee <veegee@veegee.org>
2015-11-11 02:06:36 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-VERSION = (0, 12, 3)
+VERSION = (0, 12, 4)
 __version__ = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
 __author__ = 'veegee'
 __maintainer__ = 'veegee'
